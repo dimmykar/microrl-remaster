979c4dc4f1c97ab60ea0c54b66b5b9a14790c6cb
Dmitry Karasev <karasevsdmitry@yandex.ru>
2022-01-09 12:15:34 +0300

    Fix tab spaces

--- examples/stm32_example/src/stm32_misc/stm32_misc.c
+++ examples/stm32_example/src/stm32_misc/stm32_misc.c
@@ -76,35 +76,35 @@ uint8_t  passw_in = 0;
  * \brief           Init STM32F4 platform
  */
 void init(void) {
-  LL_USART_InitTypeDef USART_InitStruct = {0};
-  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+    LL_USART_InitTypeDef USART_InitStruct = {0};
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
   
-  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
-  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART4);
-  __DSB();
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_UART4);
+    __DSB();
 
-  GPIO_InitStruct.Pin = UART_TX_Pin | UART_RX_Pin;
-  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
-  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
-  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-  GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
-  LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = UART_TX_Pin | UART_RX_Pin;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+    GPIO_InitStruct.Alternate = LL_GPIO_AF_8;
+    LL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  USART_InitStruct.BaudRate = 115200;
-  USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
-  USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
-  USART_InitStruct.Parity = LL_USART_PARITY_NONE;
-  USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
-  USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
-  USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
-  LL_USART_Init(UART4, &USART_InitStruct);
+    USART_InitStruct.BaudRate = 115200;
+    USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
+    USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
+    USART_InitStruct.Parity = LL_USART_PARITY_NONE;
+    USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+    USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+    USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
+    LL_USART_Init(UART4, &USART_InitStruct);
 
-  LL_USART_DisableIT_CTS(UART4);
+    LL_USART_DisableIT_CTS(UART4);
 
-  LL_USART_ConfigAsyncMode(UART4);
+    LL_USART_ConfigAsyncMode(UART4);
 
-  LL_USART_Enable(UART4);
+    LL_USART_Enable(UART4);
 }
 
 /**
@@ -447,6 +447,6 @@ void post_exec_hook(microrl_t* mrl, int res, int argc, const char* const *argv) 
         print(mrl, "Command exited with status ");
         print(mrl, str);
         print(mrl, _ENDLINE_SEQ);
-  }
+    }
 }
 #endif /* MICRORL_CFG_USE_COMMAND_HOOKS */
